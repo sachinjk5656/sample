23c91561a1a73e7c0eda168cc2e4b6be00b844f4
Sachin J K <sachinaws5656@gmail.com>
2022-07-15 22:23:20 +0530

    adding new files

---
+++ dummy/sample3.java
@@ -0,0 +1,11 @@
+public class MyClass {
+  static void myMethod() {
+    System.out.println("Hello World welcome to sample repository!");
+  }
+
+  public static void main(String[] args) {
+    myMethod();
+  }
+}
+
+// Outputs "Hello World!"
